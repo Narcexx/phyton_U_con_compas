611a2862b233593ba6f9ad4c6df5935b93546e00
Manu_PC_U <manuel.rubilar@alu.unach.cl>
2025-11-03 14:41:54 -0300

    creacion interfaz con pygame

---
+++ sabana/vista/interfaz.py
@@ -0,0 +1,79 @@
+import pygame
+import random
+import os
+
+class Interfaz:
+    def __init__(self, sabana):
+        pygame.init()
+        self.sabana = sabana
+        self.tamaño_celda = 32
+        self.ancho_pantalla = sabana.ancho * self.tamaño_celda
+        self.alto_pantalla = sabana.alto * self.tamaño_celda
+        self.pantalla = pygame.display.set_mode((self.ancho_pantalla, self.alto_pantalla))
+        pygame.display.set_caption("Sabana")
+        self.reloj = pygame.time.Clock()
+
+        self.fondo = pygame.image.load(os.path.join("imagenes", "sabana.png"))
+        self.fondo = pygame.transform.scale(self.fondo, (self.ancho_pantalla, self.alto_pantalla))
+
+        self.imagenes_animales = {
+            "Elefante_M": pygame.image.load(os.path.join("imagenes", "elefante_macho.png")),
+            "Elefante_F": pygame.image.load(os.path.join("imagenes", "elefante_hembra.png")),
+            "Leon_M": pygame.image.load(os.path.join("imagenes", "leon_macho.png")),
+            "Leon_F": pygame.image.load(os.path.join("imagenes", "leon_hembra.png")),
+            "Chimpance_M": pygame.image.load(os.path.join("imagenes", "chimpance_macho.png")),
+            "Chimpance_F": pygame.image.load(os.path.join("imagenes", "chimpance_hembra.png")),
+        }
+
+        self.imagenes_comidas = {
+            "pasto": pygame.image.load(os.path.join("imagenes", "pasto.png")),
+            "carne": pygame.image.load(os.path.join("imagenes", "carne.png")),
+            "insectos": pygame.image.load(os.path.join("imagenes", "insectos.png")),
+        }
+
+        # redimensiona todas las imagenes
+        for clave in self.imagenes_animales:
+            self.imagenes_animales[clave] = pygame.transform.scale(self.imagenes_animales[clave], (40, 40))
+        for clave in self.imagenes_comidas:
+            self.imagenes_comidas[clave] = pygame.transform.scale(self.imagenes_comidas[clave], (25, 25))
+
+    # dibuja todo en pantalla
+    def dibujar(self):
+        self.pantalla.blit(self.fondo, (0, 0))
+
+        # dibujar comidas
+        for comida in self.sabana.comidas:
+            x, y = comida.posicion
+            img = self.imagenes_comidas[comida.tipo]
+            self.pantalla.blit(img, (x * self.tamaño_celda, y * self.tamaño_celda))
+
+        # dibujar animales
+        for animal in self.sabana.animales:
+            if not animal.vivo:
+                continue
+            x, y = animal.posicion
+            clave = f"{animal.__class__.__name__}_{animal.sexo}"
+            img = self.imagenes_animales.get(clave)
+            if img:
+                self.pantalla.blit(img, (x * self.tamaño_celda, y * self.tamaño_celda))
+
+        pygame.display.flip()
+
+    def ejecutar(self):
+        corriendo = True
+        while corriendo:
+            for evento in pygame.event.get():
+                if evento.type == pygame.QUIT:
+                    corriendo = False
+
+            # actualizar logica del mundo
+            self.sabana.mover_hacia_comida()
+            self.sabana.reproducir()
+
+            # redibujar pantalla
+            self.dibujar()
+
+            # controlar velocidad (60 FPS)
+            self.reloj.tick(60)
+
+        pygame.quit()
